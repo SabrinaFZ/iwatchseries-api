db33b64f6c4c966cea637256e1d8af7384299598
Sabrina Fernandez Zambrano <sabrina.fer.zam@gmail.com>
2020-10-24 13:31:09 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# iwatchseries-api
---
+++ src/app.js
@@ -0,0 +1,29 @@
+
+const dotenv = require('dotenv')
+const express = require('express')
+
+const env = dotenv.config();
+if (env.error) {
+    throw new Error("Couldn't find .env file");
+}
+
+const host = process.env.host || 'http://localhost'
+const port = process.env.port || '3000'
+
+function startServer() {
+    const app = express()
+    app.listen(port, () => {
+        console.log(`Server is running ---> ${host}:${port}`)
+    })
+    .on('error', err => {
+        process.exit(1);
+    });
+}
+
+startServer();
+
+
+
+
+
+
