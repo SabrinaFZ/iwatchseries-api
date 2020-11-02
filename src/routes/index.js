a9b5445d972b4c6435e2c70e64a1141ab927e91c
Sabrina Fernandez Zambrano <sabrina.fer.zam@gmail.com>
2020-11-02 12:53:07 +0100

    Secure user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../middlewares/auth/auth.middleware')
 const authRoutes = require('./auth/auth.route')
 const usersRoutes = require('./users/users.route')
 
 router.use('/auth', authRoutes)
-router.use('/users', usersRoutes)
+router.use('/users', authMiddleware, usersRoutes)
 
 module.exports = router
