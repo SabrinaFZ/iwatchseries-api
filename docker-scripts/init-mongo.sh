c8220924f45c783b6c7c4eadec9949121f214d18
Sabrina Fernandez Zambrano <sabrina.fer.zam@gmail.com>
2020-11-08 19:25:50 +0100

    Modify init mongo script

--- docker-scripts/init-mongo.sh
+++ docker-scripts/init-mongo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Creating mongo user..."
-mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD ${MONGO_PASSWORD} --authenticationDatabase admin --eval "db=db.getSiblingDB(${MONGO_DB});db.createUser({user: 'admin', pwd: 'admin', roles: [{role: 'readWrite', db: ${MONGO_DB}}]});"
+mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --host localhost --port 27017 --authenticationDatabase admin --eval "db=db.getSiblingDB(${MONGO_INITDB_DATABASE});db.createUser({user: ${MONGO_INITDB_ROOT_USERNAME}, pwd: ${MONGO_INITDB_ROOT_PASSWORD}, roles: [{role: 'readWrite', db: ${MONGO_INITDB_DATABASE}}]});"
 echo "Mongo admin user created."
